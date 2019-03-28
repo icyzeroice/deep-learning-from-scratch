5b0536f2bc32cf8c9194ec25055209fd106e0e0e
icyzeroice <ziv3@outlook.com>
2019-03-28 23:53:02 +0800

    update note

--- ch04/two_layer_net.py
+++ ch04/two_layer_net.py
@@ -9,6 +9,7 @@ class TwoLayerNet:
 
     def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
         # 重みの初期化
+        # 参数列表的初始化
         self.params = {}
         self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
         self.params['b1'] = np.zeros(hidden_size)
@@ -16,60 +17,105 @@ class TwoLayerNet:
         self.params['b2'] = np.zeros(output_size)
 
     def predict(self, x):
+        """计算当前输出
+        Args:
+            x: 输入的图像数据
+        Returns:
+            output
+        """
+
         W1, W2 = self.params['W1'], self.params['W2']
         b1, b2 = self.params['b1'], self.params['b2']
-    
+
         a1 = np.dot(x, W1) + b1
         z1 = sigmoid(a1)
         a2 = np.dot(z1, W2) + b2
         y = softmax(a2)
-        
+
         return y
-        
+
     # x:入力データ, t:教師データ
     def loss(self, x, t):
+        """计算损失函数
+        Args:
+            x: 输入的图像数据
+            t: 正确解的标签
+        Returns:
+            cross entropy error between output & labeled true answer
+        """
+
         y = self.predict(x)
-        
+
         return cross_entropy_error(y, t)
-    
+
     def accuracy(self, x, t):
+        """计算识别的精度
+        Args:
+            x: 输入的图像数据
+            t: 正确解的标签
+
+        Returns:
+            计算结果检测数据的准确度
+        """
+
         y = self.predict(x)
         y = np.argmax(y, axis=1)
         t = np.argmax(t, axis=1)
-        
+
         accuracy = np.sum(y == t) / float(x.shape[0])
         return accuracy
-        
+
     # x:入力データ, t:教師データ
     def numerical_gradient(self, x, t):
+        """计算权重参数的梯度
+        Description:
+            这里计算损失函数的梯度，得到的梯度用来计算新的系数 W 和偏置 b
+
+        Args:
+            x: 输入的图像数据
+            t: 正确解的标签
+
+        Returns:
+            梯度
+        """
+
         loss_W = lambda W: self.loss(x, t)
-        
+
         grads = {}
         grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
         grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
         grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
         grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
-        
+
         return grads
-        
+
     def gradient(self, x, t):
+        """计算权重参数的梯度（高速版）
+        Args:
+            x: 输入的图像数据
+            t: 正确解的标签
+
+        Returns:
+            梯度
+        """
+
         W1, W2 = self.params['W1'], self.params['W2']
         b1, b2 = self.params['b1'], self.params['b2']
         grads = {}
-        
+
         batch_num = x.shape[0]
-        
+
         # forward
         a1 = np.dot(x, W1) + b1
         z1 = sigmoid(a1)
         a2 = np.dot(z1, W2) + b2
         y = softmax(a2)
-        
+
         # backward
         dy = (y - t) / batch_num
         grads['W2'] = np.dot(z1.T, dy)
         grads['b2'] = np.sum(dy, axis=0)
-        
+
         dz1 = np.dot(dy, W2.T)
         da1 = sigmoid_grad(a1) * dz1
         grads['W1'] = np.dot(x.T, da1)
